a5b1d809cd553eb26dcfded811d9aa2a8a8bc47b
William Johnson dos Santos Okano <williamokano@gmail.com>
2021-05-02 03:43:31 +0200

    added integration test for controller

---
+++ src/test/kotlin/dev/okano/mahatest/rest/CheckoutControllerIntegrationTest.kt
@@ -0,0 +1,55 @@
+package dev.okano.mahatest.rest
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.util.stream.Stream
+
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class CheckoutControllerIntegrationTest {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+    @ParameterizedTest
+    @MethodSource("argumentsProvider")
+    fun `should calculate total correctly`(productList: List<String>, expectedPrice: Int) {
+        mockMvc.perform(
+            post("/checkout")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(productList))
+        )
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.price").value(expectedPrice))
+    }
+
+    companion object {
+        @JvmStatic
+        fun argumentsProvider() = Stream.of(
+            Arguments.of(listOf("001"), 100),
+            Arguments.of(listOf("001", "001"), 200),
+            Arguments.of(listOf("001", "001", "001"), 200),
+            Arguments.of(listOf("001", "002", "003", "004"), 260),
+            Arguments.of(listOf("001", "002", "003", "004", "002"), 300),
+            Arguments.of(listOf("001"), 100),
+            Arguments.of(emptyList<String>(), 0)
+        )
+    }
+}
